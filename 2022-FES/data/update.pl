3479bc3441654474cda4566f1be292dadaf7bc54
slowe <299787+slowe@users.noreply.github.com>
2022-09-22 15:57:45 +0100

    Update building blocks
    Use GeoJSON groups to sum up building block data.

--- 2022-FES/data/update.pl
+++ 2022-FES/data/update.pl
@@ -1,4 +1,10 @@
 #!/usr/bin/perl
+# This script will download and process FES data.
+# It gets the building block definitions, the building block data, and the GSP info files.
+# The 2022 building block definitions now have a "Included in ESO Data" column.
+# Call this with:
+#    perl update.pl
+#    perl update.pl download
 
 use warnings;
 use utf8;
@@ -6,47 +12,48 @@ use Cwd qw(abs_path);
 use Data::Dumper;
 use Text::CSV;
 use JSON::XS;
+use POSIX qw(strftime);
 use open qw( :std :encoding(UTF-8) );
 
 # Get the real base directory for this script
 my $basedir = "./";
 if(abs_path($0) =~ /^(.*\/)[^\/]*/){ $basedir = $1; }
 
-print "Processing FES data\n";
-print "===================\n";
-
 
 $cache = 1;
 if(@ARGV && $ARGV[0] eq "download"){ $cache = 0; }
-
 
 $workdir = $basedir."temp/";
 if(!-d $workdir){
 	`mkdir $workdir`;
 }
 
+logIt("Processing FES data");
+logIt("===================");
+
+
 
 # https://data.nationalgrideso.com/future-energy-scenarios/future-energy-scenario-fes-building-block-data
 # https://data.nationalgrideso.com/future-energy-scenarios/regional-breakdown-of-fes-data-electricity/r/grid_supply_point_info
 $csvs = {
 	"bb-defs"=>{
-		"url"=>"",
-		"file"=>$workdir."building_block_definitions.csv"
+		"url"=>"https://data.nationalgrideso.com/backend/dataset/30df2649-99cf-4f84-9128-6c58fc1ea72a/resource/e5ab7ecb-0ab1-4fe7-833c-1fe905b086f8/download/building-block-definitions-2022.csv",
+		"file"=>$workdir."building-block-definitions.csv"
 	},
 	"bb"=>{
-		"url"=>"",
-		"file"=>$workdir."building_block_data.csv"
+		"url"=>"https://data.nationalgrideso.com/backend/dataset/30df2649-99cf-4f84-9128-6c58fc1ea72a/resource/36fd3aa9-6e42-418f-b1bb-a31bbfcf2008/download/fes-2022-building-blocks-version-2.0.csv",
+		"file"=>$workdir."building-block-data.csv"
 	},
 	"gsp"=>{
 		"url"=>"https://data.nationalgrideso.com/backend/dataset/963525d6-5d83-4448-a99c-663f1c76330a/resource/41fb4ca1-7b59-4fce-b480-b46682f346c9/download/fes2021_regional_breakdown_gsp_info.csv",
-		"file"=>$workdir."gsp_info.csv"
+		"file"=>$workdir."gsp-info.csv"
 	}
 };
 
 # Get an update of the package file from the ESO data portal
 $file = $workdir."datapackage.json";
 $url = "https://data.nationalgrideso.com/future-energy-scenarios/future-energy-scenario-fes-building-block-data/datapackage.json";
-print "Getting package from $url\n";
+logIt("Getting package from $url");
 `curl -L -s $url > $file`;
 $json = loadJSON($file);
 # Go through resources from the most recent and find URLs of CSV files
@@ -66,7 +73,7 @@ for($i = $n-1; $i >= 0; $i--){
 for $key (sort(keys(%{$csvs}))){
 	$file = $csvs->{$key}{'file'};
 	if(!$cache || !-e $csvs->{$key}{'file'} || -s $csvs->{$key}{'file'} == 0){
-		print "Downloading CSV from $csvs->{$key}{'url'}\n";
+		logIt("Downloading CSV from $csvs->{$key}{'url'}");
 		`wget -q --no-check-certificate -O $csvs->{$key}{'file'} "$csvs->{$key}{'url'}"`;
 	}
 }
@@ -79,18 +86,60 @@ for $key (sort(keys(%{$csvs}))){
 # Double check GSPs for duplicate names
 %gspnames = ();
 for $gsp (sort(keys(%gsps))){
-	if($gspnames{$gsps{$gsp}{'Name'}}){
-		print "WARNING: Ambiguity for GSP name \"$gsps{$gsp}{'Name'}\" - assigned to $gsp and $gspnames{$gsps{$gsp}{'Name'}}.\n";
+	$prevgsp = $gspnames{$gsps{$gsp}{'Name'}};
+
+	if($prevgsp){
+		logIt("WARNING: Ambiguity for GSP name \"$gsps{$gsp}{'Name'}\" - assigned to $gsp and $prevgsp.");
+
 		$gsps{$gsp}{'ambiguous'} = 1;
 		$gsps{$gspnames{$gsps{$gsp}{'Name'}}}{'ambiguous'} = 1;
+
+		# Update previously matched name
+		# Remove previous name from dictionary
+		delete $gspnames{$gsps{$prevgsp}{'Name'}};
+
+		# Append a bracketed group to the name
+		$gsps{$prevgsp}{'Name'} .= " (".$gsps{$prevgsp}{'GSP Group'}.")";
+		# Add the GSP to the names dictionary with the new name
+		$gspnames{$gsps{$prevgsp}{'Name'}} = $prevgsp;
+		logIt("Updating name of $prevgsp to \"$gsps{$prevgsp}{'Name'}\"");
+
+		# Update the current name
+		$gsps{$gsp}{'Name'} .= " (".$gsps{$gsp}{'GSP Group'}.")";
+		logIt("Updating name of $gsp to \"$gsps{$gsp}{'Name'}\"");
 	}
 	$gspnames{$gsps{$gsp}{'Name'}} = $gsp;
 }
 
+# Update the GeoJSON file with names
+open(GEO,"maps/gridsupplypoints-unique-all-simplified.geojson");
+@lines = <GEO>;
+close(GEO);
+%groupedgsp = ();
+for($i = 0; $i < @lines; $i++){
+	if($lines[$i] =~ /\"properties\":\{\"GSP\":\"([^\"]+)\"([^\}]*)\}/){
+		$gsp = $1;
+		@gspbits = split(/;/,$gsp);
+		print "$gsp\n";
+		$name = "";
+		for($g = 0; $g < @gspbits; $g++){
+			print "\t$g = $gspbits[$g] - $gsps{$gspbits[$g]}{'Name'}\n";
+			$name .= ($name ? " / " : "").$gsps{$gspbits[$g]}{'Name'};
+			$groupedgsp{$gspbits[$g]} = $gsp;
+		}
+		print $name."\n";
+		$lines[$i] =~ s/\"properties\":\{\"GSP\":\"([^\"]+)\"([^\}]*)\}/\"properties\":\{\"GSP\":\"$1\",\"Name\":\"$name\"\}/;
+		print $lines[$i];
+	}
+}
+open(GEO,">","maps/gridsupplypoints-unique-all-simplified.geojson");
+print GEO @lines;
+close(GEO);
 
 ##################
 # Building Blocks
 
+# Load our parameter definition file
 $parameters = loadJSON($basedir.'scenarios/parameters-building-blocks.json');
 
 # STEP 1 - process the building block definitions
@@ -114,10 +163,10 @@ $parameters = loadJSON($basedir.'scenarios/parameters-building-blocks.json');
 });
 for $id (sort(keys(%bbdef))){
 	$name = "";
+	# Construct a name to use for this building block
 	if($bbdef{$id}{'Template'} eq "Generation"){
 		$name = $bbdef{$id}{'Technology'}.": ".$bbdef{$id}{'Technology Detail'}." (".$bbdef{$id}{'Units'}.") ".$bbdef{$id}{'Detail'};
 	}elsif($bbdef{$id}{'Template'} eq "Demand"){
-		#$name = $bbdef{$id}{'Template'}.": ".$bbdef{$id}{'Technology Detail'}." (".$bbdef{$id}{'Units'}.") ".$bbdef{$id}{'Detail'};
 		$name = $bbdef{$id}{'Technology Detail'}." (".$bbdef{$id}{'Units'}.") ".$bbdef{$id}{'Detail'};
 	}elsif($bbdef{$id}{'Template'} eq "Demand Low Carbon Technologies"){
 		$name = $bbdef{$id}{'Technology'}.": ".$bbdef{$id}{'Technology Detail'}." (".$bbdef{$id}{'Units'}.")";#." ".$bbdef{$id}{'Detail'};
@@ -140,21 +189,24 @@ for $id (sort(keys(%bbdef))){
 		$parameters->[$match]{"optgroup"} = $bbdef{$id}{'Template'};
 	}else{
 		# Create a new entry
-		print "Add new $id\n";
+		logIt("Add new $id");
 		push(@{$parameters},{"key"=> $id, "title"=>$name, "optgroup"=> $bbdef{$id}{'Template'}, "combine"=>"sum", "dp"=>1, "description"=>$desc});
 		$match = @{$parameters} - 1;
 	}
 
-	if($bbdef{$id}{'ESO Comments'} =~ /Not included/i || $bbdef{$id}{'ESO Comments'} =~ /We are unable to redistribute this data/){
-		print "WARNING: Excluding $id because the ESO Comments say it isn't there.\n";
+	# Only include building blocks that are explicitly included in the ESO Data
+	if($bbdef{$id}{'Included in ESO Data'} ne "Y"){
+		logIt("WARNING: Excluding $id because the 'Included in ESO Data' column is \"$bbdef{$id}{'Included in ESO Data'}\".");
 		splice(@{$parameters},$match,1);
 	}
 
 
 }
+
+# Save the updated parameter configuration
 $file = $basedir.'scenarios/parameters-building-blocks.json';
 saveJSON($file,$parameters,{'postprocess'=>\&cleanParameters,'name'=>'parameters configuration'});
-print "NOTE: You should check if this looks sensible. If this code has generated new parameters from the data you should make sure that the 'combine' method for each is set correctly to either \"max\" or \"sum\".\n";
+logIt("NOTE: You should check if this looks sensible. If this code has generated new parameters from the data you should make sure that the 'combine' method for each is set correctly to either \"max\" or \"sum\".");
 
 if(!-d $workdir."building-blocks/"){
 	$dir = $workdir."building-blocks/";
@@ -169,40 +221,70 @@ while(($filename = readdir($dh))){
 	}
 }
 closedir($dh);
-print "Reading $file\n";
+logIt("Reading $file");
 @rows = getCSV($csvs->{'bb'}{'file'},{
 	'map'=>{
 		'Building Block ID Number'=>'BBID'
 	}
 });
-$ystart = 2020;
+
+$ystart = 2021;
 $yend = 2050;
 
 my %data;
+my %gspvalues;
+my $gid;
+
 for($i = 0; $i < @rows; $i++){
-	$scenario = $rows[$i]{'FES Scenario'};
-	$scenario =~ s/[a-z\s]//g;
-	$scenario = substr($scenario,0,2);
+	$scenario = getShortCode($rows[$i]{'FES Scenario'});
 	$bb = $rows[$i]{'BBID'};
 
-	if(!$data{$scenario}){
-		$data{$scenario} = {};
+	if(!$gspvalues{$scenario}){
+		$gspvalues{$scenario} = {};
 	}
-	if(!$data{$scenario}{$bb}){
-		$data{$scenario}{$bb} = {};
+	if(!$gspvalues{$scenario}{$bb}){
+		$gspvalues{$scenario}{$bb} = {};
 	}
+
 	$id = $gspnames{$rows[$i]{'GSP'}};
+
 	if(!$id){
-		print "WARNING: In $scenario/$bb No ID for \"$rows[$i]{'GSP'}\"\n";
+		logIt("WARNING: In $scenario/$bb No ID for \"$rows[$i]{'GSP'}\"");
 		$id = $rows[$i]{'GSP'};
 		$gspnames{$rows[$i]{'GSP'}} = $rows[$i]{'GSP'};
 	}
-	if($data{$scenario}{$bb}{$id}){
-		print "WARNING: In $scenario/$bb Already got \"$id\"\n";
-	}else{
-		$data{$scenario}{$bb}{$id} = "";
+
+	$gid = $groupedgsp{$id};
+	if($gid){
+		
+		if(!$gspvalues{$scenario}{$bb}{$gid}){ $gspvalues{$scenario}{$bb}{$gid} = {}; }
+
 		for($yy = $ystart; $yy <= $yend; $yy++){
-			$data{$scenario}{$bb}{$id} .= ",".($rows[$i]{$yy}||"");
+			if(!$gspvalues{$scenario}{$bb}{$gid}{$yy}){ $gspvalues{$scenario}{$bb}{$gid}{$yy} = 0; }
+
+
+			if($rows[$i]{$yy}){
+				$gspvalues{$scenario}{$bb}{$gid}{$yy} += $rows[$i]{$yy};
+			}
+		}
+	}else{
+		logIt("WARNING: No group ID for $id");
+	}
+
+}
+foreach $scenario (keys(%gspvalues)){
+	foreach $bb (keys(%{$gspvalues{$scenario}})){
+		if(!$data{$scenario}){
+			$data{$scenario} = {};
+		}
+		if(!$data{$scenario}{$bb}){
+			$data{$scenario}{$bb} = {};
+		}
+		foreach $id (keys(%{$gspvalues{$scenario}{$bb}})){
+			$data{$scenario}{$bb}{$id} = "";
+			for($yy = $ystart; $yy <= $yend; $yy++){
+				$data{$scenario}{$bb}{$id} .= ",".($gspvalues{$scenario}{$bb}{$id}{$yy}||"");
+			}
 		}
 	}
 }
@@ -212,14 +294,12 @@ $scenarios = loadJSON($basedir.'scenarios/index-building-blocks.json');
 %scenariolookup;
 
 for($s = 0; $s < @{$scenarios} ; $s++){
-	$scode = $scenarios->[$s]{'key'};
-	$scode =~ s/([A-Za-z])[a-z]+(\s|$)/$1/g;
-	$scode = uc(substr($scode,0,2));
+	$scode = getShortCode($scenarios->[$s]{'key'});
 	$scenariolookup{$scenarios->[$s]{'key'}} = $scode;
 	for($i = 0; $i < @{$parameters}; $i++){
 		if($parameters->[$i]{'key'}){
 			if(!$data{$scode}{$parameters->[$i]{'key'}}){
-				print "WARNING: No building block data seems to exist for $scode $parameters->[$i]{'key'}\n";
+				logIt("WARNING: No building block data seems to exist for $scode $parameters->[$i]{'key'}");
 			}
 		}
 	}
@@ -241,11 +321,11 @@ for $scenario (sort(keys(%data))){
 		close($fh);
 	}
 
-	print "Scenario: $scenario\n";
+	logIt("Scenario: $scenario");
 	for($s = 0; $s < @{$scenarios} ; $s++){
 		$scode = $scenarios->[$s]{'key'};
 		if($scenariolookup{$scode} eq $scenario){
-			print "\tUpdating $scode data\n";
+			logIt("\tUpdating $scode data");
 			$scenarios->[$s]{'data'} = {};
 			for $bb (sort(keys(%{$data{$scenario}}))){
 				#"demandpk-all" : {"dataBy" : "gsp","file" : "gridsupplypoints/SP-DemandPk-All.csv","key" : "Primary"},
@@ -270,6 +350,13 @@ saveJSON($file,$scenarios,{'postprocess'=>\&cleanScenarios,'name'=>'scenarios co
 
 ##########################
 # SUBROUTINES
+
+sub getShortCode {
+	my $str = $_[0];
+	$str =~ s/([A-Za-z])[a-z]+(\s|$)/$1/g;
+	$str = uc(substr($str,0,2));
+	return $str;
+}
 
 sub getCSV {
 	my (@lines,@header,%datum,$c,$i,$id,@data,%dat,$startline,$line,@cols,$row,%prev);
@@ -381,7 +468,7 @@ sub saveJSON {
 	}
 
 	# Save the event file
-	print "Saving ".($conf->{'name'} || "JSON")." to $file\n";
+	logIt("Saving ".($conf->{'name'} || "JSON")." to $file");
 	open(FILE,">",$file);
 	print FILE $str;
 	close(FILE);
@@ -407,4 +494,13 @@ sub cleanParameters {
 	$str =~ s/\n\t\}/\}/g;
 	$str =~ s/(\t\{)(.*)\,("key": ?"[^\"]+","optgroup": ?"[^\"]+","title": ?"[^\"]+")/$1$3,$2/g;
 	return $str;
+}
+
+sub logIt {
+	my ($fh,$str);
+	$str = $_[0];
+	open($fh,">>",$workdir."update.log");
+	print $fh strftime("%FT%H:%M:%S", gmtime).":\t$str\n";
+	print $str."\n";
+	close($fh);
 }
